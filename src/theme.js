69067803f989697a2bc75a2f95928a0de3fe3d62
tphatwebdev <trantienphat13112004@gmail.com>
2026-08-11 14:58:29 +0000

    clear comment

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,4 @@
-// import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 import { createTheme } from '@mui/material/styles'
-// Create a theme instance.
 const theme = createTheme({
   trello: {
     appBarHeight: '58px',
