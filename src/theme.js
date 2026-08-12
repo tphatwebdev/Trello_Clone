34291b6d64f932e23a74d3d75d48d9a0d35448ed
tphatwebdev <trantienphat13112004@gmail.com>
2026-08-12 07:54:01 +0000

    Refactor project again

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,6 @@
 import { createTheme } from '@mui/material/styles'
-
+const COLUMN_HEADER_HEIGHT = '50px'
+const COLUMN_FOOTER_HEIGHT = '56px'
 const APP_BAR_HEIGHT = '58px'
 const BOARD_BAR_HEIGHT = '60px'
 const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
@@ -8,7 +9,9 @@ const theme = createTheme({
   trello: {
     appBarHeight: APP_BAR_HEIGHT,
     boardBarHeight: BOARD_BAR_HEIGHT,
-    boardContentHeight: BOARD_CONTENT_HEIGHT
+    boardContentHeight: BOARD_CONTENT_HEIGHT,
+    columnHeaderHeight: COLUMN_HEADER_HEIGHT,
+    columnFooterHeight: COLUMN_FOOTER_HEIGHT
   },
   colorSchemes: {
     light: {},
